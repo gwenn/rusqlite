de7853a32623ca109325bb7099736c6602a9228a
gwenn <gtreguier@gmail.com>
2016-11-05 16:27:42 +0100

    Fix benches

--- benches/lib.rs
+++ benches/lib.rs
@@ -4,7 +4,6 @@ extern crate test;
 extern crate rusqlite;
 
 use rusqlite::Connection;
-use rusqlite::cache::StatementCache;
 use test::Bencher;
 
 #[bench]
@@ -17,7 +16,6 @@ fn bench_no_cache(b: &mut Bencher) {
 #[bench]
 fn bench_cache(b: &mut Bencher) {
     let db = Connection::open_in_memory().unwrap();
-    let cache = StatementCache::new(&db, 15);
     let sql = "SELECT 1, 'test', 3.14 UNION SELECT 2, 'exp', 2.71";
-    b.iter(|| cache.get(sql).unwrap());
+    b.iter(|| db.prepare_cached(sql).unwrap());
 }
